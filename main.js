f29336cc6b0405f4bfbe25efbfa4fc77bc1b2ece
mhmd <ms.mhmdsalah@gmail.com>
2024-05-31 16:21:59 +0300

    add feather

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 let container = document.querySelector(".container");
 let colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
-let SQUARES = 700;
+let SQUARES = 1008;
 
-
-for(let i = 0;i<SQUARES;i++){
-  let square = document.createElement("div")
-  square.classList.add("square")
+// create squares
+for (let i = 0; i < SQUARES; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
   square.addEventListener("mouseover", () => setColor(square));
   square.addEventListener("mouseout", () => removeColor(square));
-  container.appendChild(square)
+  container.appendChild(square);
 }
 
-function setColor(element){
+function setColor(element) {
   let color = getRandomColor();
-  element.style.background=color;
-  element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
 }
 
-function removeColor(element){
-  element.style.background = "#1d1d1d"
+function removeColor(element) {
+  element.style.background = "#1d1d1d";
   element.style.boxShadow = `0 0 2px #111`;
 }
-function getRandomColor(){
+function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)];
 }
+let squaresDiv = [...document.querySelectorAll(".square")];
+
+function test(){
+  let color = getRandomColor();
+  let randomSquare = squaresDiv[Math.floor(Math.random() * squaresDiv.length)]
+    randomSquare.style.background = color;
+    randomSquare.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
+}
+setInterval(test,100)
